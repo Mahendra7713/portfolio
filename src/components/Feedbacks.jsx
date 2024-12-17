5746c60fcc9491e435f45b73d5b9d2c37585c7a2
Mahi <mahendra@codesis.tech>
2024-12-18 01:47:54 +0530

    added certificates

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -5,38 +5,57 @@ import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 import { certificates } from "../constants";
+import { github } from "../assets";
 
 const FeedbackCard = ({
   index,
-  description,
   name,
-  designation,
   institute,
   image,
+  link,
+  github,
+  logo
 }) => (
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
-    className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
+    className='bg-black-200 p-5 rounded-3xl xs:w-[320px] w-full'
   >
-    <p className='text-white font-black text-[48px]'>"</p>
 
     <div className='mt-1'>
-      <p className='text-white tracking-wider text-[18px]'>{description}</p>
+      <div className='relative w-full h-[230px]'>
+        <img
+          src={image}
+          alt='project_image'
+          className='w-full h-full object-cover rounded-2xl'
+        />
 
+        <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+          <div
+            onClick={() => window.open(link, "_blank")}
+            className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+          >
+            <img
+              src={github}
+              alt='source code'
+              className='w-1/2 h-1/2 object-contain'
+            />
+          </div>
+        </div>
+      </div>
       <div className='mt-7 flex justify-between items-center gap-1'>
         <div className='flex-1 flex flex-col'>
           <p className='text-white font-medium text-[16px]'>
-            <span className='blue-text-gradient'>@</span> {name}
+            <span className='blue-text-gradient'></span> {name}
           </p>
           <p className='mt-1 text-secondary text-[12px]'>
-            {designation} of {institute}
+            {institute}
           </p>
         </div>
 
         <img
-          src={image}
+          src={logo}
           alt={`feedback_by-${name}`}
-          className='w-10 h-10 rounded-full object-cover'
+          className='w-10 h-10 rounded-full object-contain'
         />
       </div>
     </div>
